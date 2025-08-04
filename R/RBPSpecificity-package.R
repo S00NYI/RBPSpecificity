263b8231409d143195298e77ce12de768819e980
SoonYi <55300935+S00NYI@users.noreply.github.com>
2025-08-03 22:03:49 -0400

    updated for release

--- R/RBPSpecificity-package.R
+++ R/RBPSpecificity-package.R
@@ -3,29 +3,28 @@
 #' @description
 #' The `RBPSpecificity` package provides a suite of tools for researchers
 #' working with RNA Binding Proteins (RBPs) to analyze binding specificity from
-#' high-throughput sequencing data such as eCLIP (enhanced CrossLinking and
+#' high-throughput sequencing data such as CLIP (CrossLinking and
 #' ImmunoPrecipitation) and RBNS (RNA Bind-n-Seq).
 #'
 #' It allows users to quantify K-mer enrichment within RBP binding sites,
-#' assess the Inherent Specificity (IS) of binding motifs, and evaluate the
-#' Mutational Sensitivity (MS) of these motifs to single nucleotide variations.
-#' The package aims to streamline these common bioinformatic analyses and
-#' provide clear visualizations for interpreting RBP binding characteristics.
+#' assess the inherent specificity (IS) and the mutational sensitivity (MS) of
+#' RBP towards the target motifs. The package aims to streamline bioinformatic
+#' analyses and provide clear visualizations for interpreting RBP specificity.
 #'
-#' **Note:** This package is currently under active development.
+#' **Note:** This package is under active development/development.
 #'
 #' @section Key Functions:
 #' The main workflow of the package revolves around the following exported functions:
 #' \itemize{
 #'   \item \code{\link{motifEnrichment}}: Calculates K-mer enrichment from peak data...
-#'   \item \code{\link{returnIS}}: Quantifies the Inherent Specificity (IS) of RBP...
+#'   \item \code{\link{returnIS}}: Quantifies the inherent specificity (IS) of RBP...
 #'   \item \code{\link{plotIS}}: Visualizes the distribution of motif enrichment scores with annotation...
-#'   \item \code{\link{returnMS}}: Quantifies the Mutational Sensitivity (MS) of RBP...
+#'   \item \code{\link{returnMS}}: Quantifies the mutational sensitivity (MS) of RBP...
 #'   \item \code{\link{plotMS}}: Generates plots to visualize Mutational Sensitivity
 #' }
 #'
 #' @author Soon Yi \email{cu.soonyi@gmail.com}
-#' @aliases RBPSpecificity RBPSpecificity-package # Ensures ?RBPSpecificity and ?RBPSpecificity-package work
+#' @aliases RBPSpecificity RBPSpecificity-package
 #'
 #' @seealso
 #' Useful links:
